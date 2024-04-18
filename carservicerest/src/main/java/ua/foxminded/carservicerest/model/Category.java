b8e5335e3da519b06c211f7c452df87922a23307
qscaz <qscaz@host.docker.internal>
2024-04-18 17:28:03 +0300

    ManyToMany

--- carservicerest/src/main/java/ua/foxminded/carservicerest/model/Category.java
+++ carservicerest/src/main/java/ua/foxminded/carservicerest/model/Category.java
@@ -3,18 +3,14 @@ package ua.foxminded.carservicerest.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name = "categories", schema = "carservicerest")
